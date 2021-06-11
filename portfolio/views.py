9f1fe4875fadb96a35c1865a5da63ac1a47c0d99
Saiyan <shyananahamed00@gmail.com>
2021-06-11 23:04:27 +0600

    added model class Project and its object in views.py under potfolio

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Project
 
 def home(request):
-	return render(request,'portfolio/home.html')
+	projects = Project.objects.all()
+	return render(request,'portfolio/home.html',{'projects':projects})
