e8ce5829553679e8a81430985ab283baf860d435
Saiyan <shyananahamed00@gmail.com>
2021-06-12 02:57:09 +0600

    created model,make the migration, register in admin, add model in views for the blog app

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Blog
 
 def all_blogs(request):
-	return render(request,'blog/all_blogs.html')
+	blogs = Blog.objects.order_by('-date')[:5] #use "order_by(-date)" to sorted by date and show latest blog & "[:5]" to show 5 blogs
+	return render(request,'blog/all_blogs.html', {'blogs':blogs})
