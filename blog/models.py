b31b43acc9ee4c9322615929752ebd966e49bdbe
Saiyan <shyananahamed00@gmail.com>
2021-06-13 14:42:47 +0600

    polished the sites

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,6 @@ class Blog(models.Model):
 	description = models.TextField()
 
 	date = models.DateField()
+
+	def __str__(self):
+		return self.title
--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404 #for get the blog number 
 from .models import Blog
 
 def all_blogs(request):
-	blogs = Blog.objects.order_by('-date')[:5] #use "order_by(-date)" to sorted by date and show latest blog & "[:5]" to show 5 blogs
+	blogs = Blog.objects.order_by('-date')#[:5] #use "order_by(-date)" to sorted by date and show latest blog & "[:5]" to show 5 blogs
 	return render(request,'blog/all_blogs.html', {'blogs':blogs})
 
 def detail(request,blog_id):
