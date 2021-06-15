258e138e1e163976cd0eaab4410e12523174f49b
Saiyan <shyanahamed00@gmail.com>
2021-06-16 02:42:02 +0600

    setup for local_settings.py and add runing pips file

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -129,3 +129,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('Looks like no local file. You must be on production')
